6f15c77028997630ffff3a633cda18000fb40e8e
ivankonovalov <thejadeon@vk.com>
2021-10-08 18:30:02 +0300

    Fix inhertable mistakes

--- lesson_6/cargo_train.rb
+++ lesson_6/cargo_train.rb
@@ -2,6 +2,7 @@ require_relative 'train'
 
 class CargoTrain < Train
   attr_reader :type
+  qty_instances
   def initialize(number)
     @type = 'cargo'
     super
--- lesson_6/passenger_train.rb
+++ lesson_6/passenger_train.rb
@@ -1,7 +1,8 @@
-require_relative 'train'
+require_relative 'train.rb'
 
 class PassengerTrain < Train
   attr_reader :type
+  qty_instances
   def initialize(number)
     @type = 'pass'
     super
